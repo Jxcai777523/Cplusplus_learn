95a17b16dd7f7ad7a38fac141ae7558e419b4df4
Jxcai777523 <785196815@qq.com>
2026-03-29 21:31:12 +0800

    Add files via upload

---
+++ First C++ Project/practice.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 
+//#include<stdio.h>
+#include<iostream>//包含了printf scanf函数
+//using namespace std;使用std域，就不需要使用std::
+int main()
+{
+	printf("Hello C World\n");
+	std::cout << "Hello C World\n";
+
+	return 0;
+}
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "Hello C++ World!" << endl;
+//	return 0;
+//}
