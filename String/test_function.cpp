acf6f5d8d5a38583310a2cef249a4ae7f0294e55
Jxcai777523 <785196815@qq.com>
2026-04-16 21:00:18 +0800

    Add files via upload

--- String/test_function.cpp
+++ String/test_function.cpp
@@ -31,9 +31,69 @@ void test2()
 	string s3;
 	getline(cin, s3, '*');cout << s3 << endl;
 }
+void test3()//遍历字符串
+{
+	string s1("hello world");
+	for (int i = 0;i < s1.size();i++)
+	{
+		cout << s1[i]<<' ';
+	}
+}
+void test4()//使用迭代器进行遍历
+{
+	const string s1("hello world");
+	string::const_iterator it = s1.cbegin();
+	while (it != s1.cend())
+	{
+		cout << *it << ' ';
+		it++;
+	}
+	cout << endl;
+	const string s2(s1);
+	string::const_reverse_iterator rit = s2.crbegin();//反向迭代器
+	while (rit != s2.crend())
+	{
+		cout << *rit << ' ';
+		rit++;
+	}
+}
+//范围for以及auto
+void test5()
+{
+	string s1("hello world");
+	//auto 自动识别ch变量的类型
+	//然后会让ch依次从s1的头到尾进行遍历
+	//其实范围for的底层还是迭代器
+	for (auto ch : s1)
+	{
+		cout << ch << ' ';
+	}
+}
+void test6()
+{
+	string s1("hello world");
+	s1.resize(5);
+	cout << s1 << endl;;
+	s1.resize(10, '!');
+	cout << s1;
+}
+void test7()
+{
+	/*string s1("hello world");*/
+	/*cout << s1[4] << ' ' << s1.at(4) << endl;
+	cout << s1.back() << ' ' << s1.front() << endl;*/
+	/*s1.append(s1, 5, 11);
+	cout <<s1;*/
+
+}
 int main()
 {
 	//test1();
-	test2();
+	//test2();
+	//test3();
+	//test4();
+	//test5();
+	//test6();
+	test7();
 	return 0;
 }
